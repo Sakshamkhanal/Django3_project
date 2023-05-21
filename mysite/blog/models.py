052ea126699fa6f115be691aa57812b611135b74
Saksham Khanal <saksham.khanal01@gmail.com>
2023-05-21 21:15:20 +0545

    Canonical urls for models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -22,6 +22,9 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
+
 
     class Meta:
         ordering = ('-publish',)
