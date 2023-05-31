292d93ed4a29af59a725e6c5af1e4f886a0875b2
Saksham Khanal <saksham.khanal01@gmail.com>
2023-05-31 20:15:12 +0545

    Final adjustment

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path('',views.PostListView.as_view(),name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
 ]
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('blog/',include('blog.urls',namespace='blog'))
+# ]
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls',namespace='blog'))
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
